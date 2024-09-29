c9f4dbefe28c16c97bac7cc993f0335fd2a997be
saurabhkr66 <sauravkum420@gmail.com>
2024-09-29 14:41:45 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
